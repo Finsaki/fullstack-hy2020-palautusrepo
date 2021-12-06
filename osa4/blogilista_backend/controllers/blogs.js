ff31f7cf9db5ead0670c2193903884b9c45bd286
akikop <aki.koppinen@metropolia.fi>
2021-12-06 19:19:18 +0200

    Part 4 exercises 4.13-4.14 done

--- osa4/blogilista_backend/controllers/blogs.js
+++ osa4/blogilista_backend/controllers/blogs.js
@@ -16,4 +16,17 @@ blogsRouter.post('/', async (request, response) => {
   response.json(savedBlog.toJSON())
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  //cannot use command new here, will create new unique id
+  const blog = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog.toJSON())
+})
+
 module.exports = blogsRouter
--- osa4/blogilista_backend/tests/blog_api.test.js
+++ osa4/blogilista_backend/tests/blog_api.test.js
@@ -10,86 +10,146 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('Getting blogs', () => {
 
-test('all blogs are returned', async () => {
-  const response = await helper.blogsInDb()
-  expect(response).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blogs have identifier id', async () => {
-  const response = await helper.blogsInDb()
-  for (let blog of response) {
-    expect(blog).toHaveProperty('id')
-  }
-})
-
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'How to Create React App in 5 Minutes?',
-    author: 'Anna Danilec',
-    url: 'https://www.blog.duomly.com/how-to-create-react-app-in-5-minutes/',
-    likes: 6
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain(
-    'How to Create React App in 5 Minutes?'
-  )
-})
-
-test('a blog with no likes can be added and likes are set to 0', async () => {
-  const newBlog = {
-    title: 'How to Create React App in 1 Minute?',
-    author: 'Danna Anilec',
-    url: 'https://www.blog.duomly.com/'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  const titlesAndLikes = blogsAtEnd.map(n => {
-    const container = {}
-    container.title = n.title
-    container.likes = n.likes
-    return container
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   })
-  expect(titlesAndLikes).toContainEqual(
-    { title: 'How to Create React App in 1 Minute?', likes: 0 }
-  )
+
+  test('all blogs are returned', async () => {
+    const response = await helper.blogsInDb()
+    expect(response).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blogs have identifier id', async () => {
+    const response = await helper.blogsInDb()
+    for (let blog of response) {
+      expect(blog).toHaveProperty('id')
+    }
+  })
+
 })
 
-test('a blog with no title and no url can not be added', async () => {
-  const newBlog = {
-    author: 'Danna Anilec',
-    likes: 2
-  }
+describe('Posting blogs', () => {
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'How to Create React App in 5 Minutes?',
+      author: 'Anna Danilec',
+      url: 'https://www.blog.duomly.com/how-to-create-react-app-in-5-minutes/',
+      likes: 6
+    }
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(
+      'How to Create React App in 5 Minutes?'
+    )
+  })
+
+  test('a blog with no likes can be added and likes are set to 0', async () => {
+    const newBlog = {
+      title: 'How to Create React App in 1 Minute?',
+      author: 'Some Indian Guy Probably',
+      url: 'https://www.youtube.com/'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const titlesAndLikes = blogsAtEnd.map(n => {
+      const container = {}
+      container.title = n.title
+      container.likes = n.likes
+      return container
+    })
+    expect(titlesAndLikes).toContainEqual(
+      { title: 'How to Create React App in 1 Minute?', likes: 0 }
+    )
+  })
+
+  test('a blog with no title and no url can not be added', async () => {
+    const newBlog = {
+      author: 'Danna Anilec',
+      likes: 2
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+
+})
+
+describe('Deleting blogs', () => {
+
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+
+})
+
+describe('Modifying blogs', () => {
+
+  test('a blog can be modified', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToModify = blogsAtStart[0]
+    blogToModify.likes = 76
+
+    await api
+      .put(`/api/blogs/${blogToModify.id}`)
+      .send(blogToModify)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    const titlesAndLikes = blogsAtEnd.map(n => {
+      const container = {}
+      container.title = n.title
+      container.likes = n.likes
+      return container
+    })
+    expect(titlesAndLikes).toContainEqual(
+      { title: blogToModify.title, likes: 76 }
+    )
+
+  })
+
 })
 
 afterAll(() => {
